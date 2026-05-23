12a913c327fc3e99032c05e9cb83a923cfc6498b
Simon <54853018+simonMarcotte@users.noreply.github.com>
2026-05-22 19:39:27 -0600

    enhanced ui + draggable components

---
+++ data/profile-data.js
@@ -0,0 +1,46 @@
+window.profileData = {
+  work: [
+    {
+      title: "Robinhood",
+      subtitle: "software engineer intern",
+      location: "toronto, on",
+      date: "sep 2025 - dec 2025",
+      logo: "assets/robinhoodLogo.png",
+      alt: "Robinhood"
+    },
+    {
+      title: "Amazon Web Services (AWS)",
+      subtitle: "software development engineer intern",
+      location: "vancouver, bc",
+      date: "may 2025 - aug 2025",
+      logo: "assets/awsLogo.png",
+      alt: "AWS"
+    },
+    {
+      title: "General Fusion",
+      subtitle: "software engineer intern",
+      location: "richmond, bc",
+      date: "jan 2024 - aug 2024",
+      logo: "assets/gfLogo.png",
+      alt: "General Fusion"
+    }
+  ],
+  education: [
+    {
+      title: "university of alberta",
+      subtitle: "bsc. computer engineering (co-op)",
+      location: "edmonton, ab",
+      date: "2021 - 2026",
+      logo: "assets/uofaLogo.png",
+      alt: "University of Alberta"
+    },
+    {
+      title: "lisgar collegiate institute",
+      subtitle: "high school",
+      location: "ottawa, on",
+      date: "2017 - 2021",
+      logo: "assets/lisgarLogo.png",
+      alt: "Lisgar"
+    }
+  ]
+};
